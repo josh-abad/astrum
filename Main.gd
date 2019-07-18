42eba6226fb7afe06eea24d1c95dea2fa1cff97c
Joshua Abad <yeyoan@gmail.com>
2019-07-18 22:42:14 +0800

    Dim screen on game over

--- Main.gd
+++ Main.gd
@@ -3,6 +3,11 @@ extends Node
 export (PackedScene) var Target
 
 
+func dim_screen():
+    $Tween.interpolate_property($CanvasModulate, 'color', $CanvasModulate.color, Color(0, 0, 0), 0.8, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+    $Tween.start()
+    
+    
 func _ready():
     randomize()
     $AmbientMusic.play()
@@ -17,6 +22,7 @@ func _on_Ball_dropped():
     $Ball.remove()
     $HUD.set_label('Game Over')
     $HUD.start_tween()
+    dim_screen()
 
 
 func _on_Ball_scored():
