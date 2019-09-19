11d8b352c490f9d9577ce6a181ad861383f8aa4e
Joshua Abad <yeyoan@gmail.com>
2019-09-19 21:50:33 +0800

    Increase font size

--- ScoredPopup.gd
+++ ScoredPopup.gd
@@ -2,9 +2,9 @@ extends Label
 
 
 func display(position: Vector2, score: int) -> void:
+    set_position(position)    
     $Tween.interpolate_property(self, 'modulate', modulate, Color(1, 1, 1, 1), 0.4, Tween.TRANS_QUAD, Tween.EASE_OUT)
     $Tween.start()
-    set_position(position)
     set_text("+" + str(score))
     yield(get_tree().create_timer(1), 'timeout')
     $Tween.interpolate_property(self, 'modulate', modulate, Color(1, 1, 1, 0), 0.4, Tween.TRANS_QUAD, Tween.EASE_OUT)
