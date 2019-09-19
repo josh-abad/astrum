16999b8f8d1537129ef18f36bc13f2bc55b6be9c
Joshua Abad <yeyoan@gmail.com>
2019-09-19 21:48:04 +0800

    Reflect the proper color of the planet and the spark

--- Planet.gd
+++ Planet.gd
@@ -1,20 +1,17 @@
 extends RigidBody2D
 
+signal scored(special, position)
+
+onready var special: bool = rand_range(0, 100) <= 30
+
 
 func _ready() -> void:
-    # $Sprite.set_texture(planets[randi() % planets.size()])
-    # _rand_scale()
+    $Sprite.self_modulate = Palette.PINK if special else Palette.ORANGE
     hide()
 
 
 func _on_start_game() -> void:
     queue_free()
-
-
-func _rand_scale() -> void:
-    var scale: float = rand_range(0.5, 1.0)
-    $Sprite.set_scale(Vector2(scale, scale))
-    $CollisionShape2D.set_scale(Vector2(scale, scale))
 
 
 func appear(position: Vector2) -> void:
@@ -30,18 +27,14 @@ func disappear(to_position: Vector2 = get_position()) -> void:
     $Tween.interpolate_property(self, 'position', get_position(), to_position, 1, Tween.TRANS_QUAD, Tween.EASE_OUT)
     $Tween.interpolate_property(self, 'modulate', modulate, Color(1, 1, 1, 0), 0.01, Tween.TRANS_QUAD, Tween.EASE_OUT)
     $Tween.start()
-    # Freeze.freeze()        
     yield(get_tree().create_timer(1), 'timeout')
     queue_free()
-    
-    
-func set_gravity(on: bool) -> void:
-    gravity_scale = 0.5 if on else 0.0
 
 
 func _on_Planet_body_entered(body: PhysicsBody2D) -> void:
     if body.is_in_group('Balls'):
-        body._on_Comet_body_entered(self)
+        emit_signal("scored", special, position)
+        body.play_effect()
         disappear()
         Engine.time_scale = 0.125
         yield(get_tree().create_timer(1), "timeout")
