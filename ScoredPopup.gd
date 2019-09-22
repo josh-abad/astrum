a336920004df89575810df517b23a2357c9fa081
Joshua Abad <yeyoan@gmail.com>
2019-09-22 22:48:15 +0800

    Add color

--- ScoredPopup.gd
+++ ScoredPopup.gd
@@ -1,13 +1,14 @@
 extends Label
 
 
-func display(position: Vector2, score: int) -> void:
+func display(position: Vector2, score: int, color: Color) -> void:
     set_position(position)    
-    $Tween.interpolate_property(self, 'modulate', modulate, Color(1, 1, 1, 1), 0.4, Tween.TRANS_QUAD, Tween.EASE_OUT)
+    add_color_override("font_color", color)
+    $Tween.interpolate_property(self, 'modulate', modulate, Color(1, 1, 1, 1), 0.2, Tween.TRANS_QUAD, Tween.EASE_OUT)
     $Tween.start()
     set_text("+" + str(score))
     yield(get_tree().create_timer(1), 'timeout')
-    $Tween.interpolate_property(self, 'modulate', modulate, Color(1, 1, 1, 0), 0.4, Tween.TRANS_QUAD, Tween.EASE_OUT)
+    $Tween.interpolate_property(self, 'modulate', modulate, Color(1, 1, 1, 0), 0.2, Tween.TRANS_QUAD, Tween.EASE_OUT)
     $Tween.start()
     yield(get_tree().create_timer(1), 'timeout')
     queue_free()
